b02716b4923b54d12ef4f67c51223306f844600c
mykolastepanenko <Nikolaua36@gmail.com>
2021-09-07 23:58:55 +0300

    bot init

--- bot.js
+++ bot.js
@@ -1,4 +1,17 @@
 require('dotenv').config();
+const TelegramBot = require('node-telegram-bot-api');
 
-console.clear();
-console.log(process.env.BOT_ID);
+const token = process.env.BOT_ID;
+
+const bot = new TelegramBot(token, {polling: true});
+
+bot.onText(/\/echo (.+)/, (msg, match) => {
+  const chatId = msg.chat.id;
+  const resp = match[1]; // the captured "whatever"
+  bot.sendMessage(chatId, resp);
+});
+
+bot.on('message', (msg) => {
+  const chatId = msg.chat.id;
+  bot.sendMessage(chatId, 'Received your message');
+});
